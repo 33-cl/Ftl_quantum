1e65367eb4a466d72fa434057a011cb8f5c018eb
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2026-01-11 19:20:53 +0100

    ex00: displaying the circuit

--- ex00/ex00.py
+++ ex00/ex00.py
@@ -8,12 +8,16 @@ circuit = QuantumCircuit(1)
 circuit.h(0)
 circuit.measure_all()
 
+# Display the circuit
+print(circuit.draw())
+
 # Launching simulation 500 times
 simulator = AerSimulator()
 result = simulator.run(circuit, shots=500).result()
 counts = result.get_counts()
 
-# Creating a graph for the experience and saving it
-graph = plot_histogram(counts)
-graph.savefig("superposition.png")
+# Display the results in a plot_histogram
+plot_histogram(counts)
+plt.savefig("superposition.png")
+plt.close()
 
